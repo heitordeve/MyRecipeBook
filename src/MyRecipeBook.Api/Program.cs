d2c72f5a16961492a3fa2f927a1040cb2d88a9ca
heitordeve <heitor.developer@outlook.com>
2026-07-08 10:40:56 -0300

    Config - Organizando o repositório

--- src/MyRecipeBook.Api/Program.cs
+++ src/MyRecipeBook.Api/Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -8,7 +7,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
