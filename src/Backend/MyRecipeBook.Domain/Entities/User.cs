4b1b5de9782755ae063c54ac215e93060d5d9eab
heitordeve <heitor.developer@outlook.com>
2026-07-18 11:51:13 -0300

    feat: começando a entidade USER

---
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRecipeBook.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public bool Active { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
