4cf28673e9c5aca8aff4693bc88ba0b38d246d2d
heitordeve <heitor.developer@outlook.com>
2026-07-20 14:22:02 -0300

    feat: iniciando a implementação de hash de senha com Argon2

---
+++ src/Backend/MyRecipeBook.Domain/Security/PasswordsHashing/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Domain.Security.PasswordsHashing;
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string passwordHash);
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/Security/PasswordsHashing/Argon2PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using MyRecipeBook.Domain.Security.PasswordsHashing;
+
+namespace MyRecipeBook.Infrastructure.Security.PasswordsHashing;
+
+internal sealed class Argon2PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifyPassword(string password, string passwordHash)
+    {
+        throw new NotImplementedException();
+    }
+}
