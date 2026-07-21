3035ac496ac86d6d1a0a007ffec4db71eeb12e22
heitordeve <heitor.developer@outlook.com>
2026-07-21 11:06:58 -0300

    feat: Validando Senha com Argon2id no .NET

--- src/Backend/MyRecipeBook.Infrastructure/Security/PasswordsHashing/Argon2PasswordHasher.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Security/PasswordsHashing/Argon2PasswordHasher.cs
@@ -15,15 +15,9 @@ internal sealed class Argon2PasswordHasher : IPasswordHasher
     public string HashPassword(string password)
     {
         var salt = RandomNumberGenerator.GetBytes(SALT_SIZE);
-        var passwordBytes = Encoding.UTF8.GetBytes(password);
-        var hashAlgorithm = new Argon2id(passwordBytes)
-        {
-            DegreeOfParallelism = DEGREE_OF_PARALLELISM,
-            MemorySize = MEMORY_SIZE,
-            Iterations = INTERATIONS,
-            Salt = salt
-        };
-        var hash = hashAlgorithm.GetBytes(HASH_SIZE);
+
+
+        var hash = HashPassword(password, salt);
 
         var combinedBytes = new byte[hash.Length + salt.Length];
         salt.CopyTo(combinedBytes);
@@ -34,6 +28,28 @@ internal sealed class Argon2PasswordHasher : IPasswordHasher
 
     public bool VerifyPassword(string password, string passwordHash)
     {
-        throw new NotImplementedException();
+        var combinedBytes = Convert.FromBase64String(passwordHash);
+        var salt = new byte[SALT_SIZE];
+        var hash = new byte[HASH_SIZE];
+
+        Array.Copy(combinedBytes, salt, SALT_SIZE);
+        Array.Copy(combinedBytes, SALT_SIZE, hash, 0, HASH_SIZE);
+
+        var newHash = HashPassword(password, salt);
+        return CryptographicOperations.FixedTimeEquals(hash, newHash);
+    }
+
+    private byte[] HashPassword(string password, byte[] salt)
+    {
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+        var hashAlgorithm = new Argon2id(passwordBytes)
+        {
+            DegreeOfParallelism = DEGREE_OF_PARALLELISM,
+            MemorySize = MEMORY_SIZE,
+            Iterations = INTERATIONS,
+            Salt = salt
+        };
+
+        return hashAlgorithm.GetBytes(HASH_SIZE);
     }
 }
